39be718c7d23e760e7c0717471c421064218a846
Alex Mah <maxhale4@gmail.com>
2017-01-04 19:50:58 -0500

    Added while loop that is not really a loop

--- Test/src/com/me/robotics/rpglol.java
+++ Test/src/com/me/robotics/rpglol.java
@@ -3,5 +3,9 @@ package com.me.robotics;
 public class rpglol {
 	public static void main(String args[]) {
 		System.out.println("Hello World! ");
+		while(true){
+			System.out.println("download more ram");
+			System.exit(0);
+		}
 	}
 }
